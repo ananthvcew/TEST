c5f9e283b1fbefe67c158191b9d5daa85adc0184
ananthvcew <ananth@vcew.ac.in>
2021-03-31 12:13:26 +0530

    first commit

---
+++ index1.php
@@ -0,0 +1,19 @@
+<?php
+include'header.php';
+
+?>
+<div class="trusted-section">
+      <div class="container">
+        <div class="row">
+<?php
+	echo"<ul class='nav'>";
+	for($i=1;$i<=10000;$i++){
+		echo"<li class='nav-item'>Sampath $i</li>";
+	}
+	echo"</ul>";
+
+?>
+        </div></div></div>
+<?php
+include'footer.php';
+?>
